18f9e78806274f9a579be866f0b8db474dd51f42
caranuial <caranuial@gmail.com>
2018-05-04 12:17:47 +0200

    Add files via upload

--- add_items.py
+++ add_items.py
@@ -1,68 +1,133 @@
+# -*- coding: utf-8 -*-
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item, User
 
 
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('sqlite:///mycatalog.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-# add a dummy user
-user1 = User(name="dummyUser", email="dummyuseremail")
+# add a default user
+user1 = User(name="default", email="default@user.here")
 session.add(user1)
 session.commit()
 
 # add new categories
-cat1 = Category(name="Soccer")
+cat1 = Category(name="Skis", id=1)
 session.add(cat1)
 session.commit()
 
-cat2 = Category(name="Basketball")
+cat2 = Category(name="Snowboards", id=2)
 session.add(cat2)
 session.commit()
 
-cat3 = Category(name="Baseball")
+cat3 = Category(name="Gloves", id=3)
 session.add(cat3)
 session.commit()
 
-cat4 = Category(name="Frisbee")
+cat4 = Category(name="Goggles", id=4)
 session.add(cat4)
 session.commit()
 
-cat5 = Category(name="Snowboarding")
+cat5 = Category(name="Helmets", id=5)
 session.add(cat5)
 session.commit()
 
-cat6 = Category(name="Rock Climbing")
+cat6 = Category(name="Poles", id=6)
 session.add(cat6)
 session.commit()
 
-cat7 = Category(name="Skating")
+cat7 = Category(name="Ski Boots", id=7)
 session.add(cat7)
 session.commit()
 
-cat8 = Category(name="Hockey")
+cat8 = Category(name="Snowboard Boots", id=8)
 session.add(cat8)
 session.commit()
 
 
 # add new items in each category
-item1 = Item(user_id=1, category_id=1, title="Soccer Cleats",
-             description="The Shoes")
-session.add(item1)
+item = Item(user_id=1, category_id=1, title="Elan Amphobio 16 Ti2 Fusion",
+             description="Please fasten your seatbelt because the Amphibio 16 Ti2 is about to take you on the ride of your life. The ultimate ski for groomed snow, this ski is more powerful than a twin turbo and rips long arcs at speed, yet it is nimble enough for tight tracks")
+session.add(item)
 session.commit()
 
-item2 = Item(user_id=1, category_id=1, title="Jersey", description="The Shirt")
-session.add(item2)
+item = Item(user_id=1, category_id=1, title="Elan Amphobio 14 TI Fusion",
+             description=" agility is what you desire and laying down effortless quick and nimble arcs gets your blood pumping, then prepare for the ultimate ride with the Amphibio 14 Ti. Built for quickness, designed to shatter heart monitors.")
+session.add(item)
 session.commit()
 
-item3 = Item(user_id=1, category_id=3, title="Bat", description="The bat")
-session.add(item3)
+item = Item(user_id=1, category_id=1, title="Elan Amphobio 14 TI Power Shift",
+             description="Finding one ski that does everything on groomed runs just got easier with the Amphibio 12 Ti. High speed long turns - check. Short turns on steep terrain - check. Cruisng at moderate speeds - Check. No need to keep looking at the menu - take the check")
+session.add(item)
 session.commit()
 
-item4 = Item(user_id=1, category_id=5, title="Snowboard",
-             description="The Snowboard")
-session.add(item4)
+item = Item(user_id=1, category_id=1, title="Elan Amphobio 84 Xti Fusion",
+             description="They say that 40 is the new 30, but don't believe it. 40 is still 40. BUT, the 84mm is the new 78mm! Versatile, quick, powerful and full of energy - don't let the waist width fool you. It will make you feel like you're 30 again, which is the new 20")
+session.add(item)
 session.commit()
+
+item = Item(user_id=1, category_id=1, title="Elan Amphobio 84 Ti Fusion",
+             description="Groomers, crud, powder and hard pack - pick your poison, because the Amphibio 84 Ti is the elixir. High performance enough for hard charging yet built to be forgiving and easy for those cruising runs at the end of the day. ")
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, category_id=2, title="Men's Burton Flight Attendant Snowboard", 
+			 description=" Balanced Freeride Geometry is the secret with setback camber and sidecut that are centered on your stance to create a twin freestyle feel when riding flat base.")
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, category_id=2, title="Men's Burton Name Dropper LTD Snowboard", 
+			 description="Designed to be a terrain-slaying alternative to more traditional twin shapes, the Burton Flight Attendant is a free spirit that dissects both pow and hard-pack with equal precision. ")
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, category_id=3, title="Women's Burton Glove + Gore warm technology",
+             description="Burton Glove + Gore warm technology. DRYRIDE 2L fabric, and a GUARANTEED TO KEEP YOU DRY GORE-TEX membrane protect this Thermacore insulated glove")
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, category_id=6, title="Leki VENOM SL",
+             description="The Venom SL has already won everything in the World Cup circuit. Now it's your turn. The high-strength aluminium upper segment and the strengthened-carbon lower one makes your racing experience an exciting challenge. Trigger S grips are a safe and sporty option while away.")
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, category_id=3, title="Leki WORLDCUP RACE TI S SPEED SYSTEM",
+             description="True Race Glove, used by the competitive racer. Pre-curved fingers and digital palm and finger tips allow for enhanced pole grip. Ceramic 3-D finger covers and long padded gauntlet protect in the gates. Heavy duty wristlet provides a good tight fit.")
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, category_id=7, title="Atomic Redster",
+             description="It has a 92mm narrow last for a super anatomical fit, Progressive Race Shell and World Cup Liner, our weapon of choice.")
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, category_id=7, title="Technica Firebird",
+             description="A proven performer, has podiumed in both speed and tech events, proving itself to be one of the most versatile boots on the World Cup circuit. The R9.3 has exceptional touch on the snow and a smooth progressive flex pattern.")
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, category_id=4, title="SPLASHTACULAR",
+             description="Everything is just a little extra with our goggles, and we wouldnt have it any other way")
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, category_id=4, title="Atomic REVENT L FDL HD OTG",
+             description="It is no fun skiing when you are guessing where the bumps are. So we have gone to town on the lenses in our Atomic Revent all-mountain range. In this top model we have added Fusion Double Lens (FDL), a revolutionary double lens with both lenses laminated together.")
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, category_id=8, title="Burton Rampant",
+             description="Soft yet supportive with a flex similar to the Concord Speed Zone boot, the Rampant keeps you comfortably connected without sacrificing the support, strength, or cushioning your all-out riding requires.")
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, category_id=8, title="Burton Concord",
+             description="The Burton Concord boot defies convention by combining versatile mid-range response and full-featured tech in a boot thats lighter and lasts longer than the competition.")
+session.add(item)
+session.commit()
+
--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item, User
 import random
 import string
-from flask import session
+from flask import session as login_session
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 import httplib2
@@ -15,25 +15,298 @@ from flask import make_response
 
 app = Flask(__name__)
 
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "WinterCatalog"
+
 # Prepare for database query
 engine = create_engine('sqlite:///mycatalog.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 dbsession = DBSession()
 
+# Login page
+@app.route('/login')
+def login():
+    state = ''.join(random.choice(
+        string.ascii_uppercase + string.digits)
+        for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
+
+# Google Connect
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    print(access_token)
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+    # ADD PROVIDER TO LOGIN SESSION
+    login_session['provider'] = 'google'
+
+    # see if user exists, if it doesn't make a new one
+    user_id = getUserID(data["email"])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+    login_session['logged_in'] = True
+    
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+# User Helper Functions
+
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    dbsession.add(newUser)
+    dbsession.commit()
+    user = dbsession.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = dbsession.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = dbsession.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+# DISCONNECT - Revoke a current user's token and reset their login_session
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    # Only disconnect a connected user.
+    access_token = login_session.get('access_token')
+    print(access_token)
+    if access_token is None:
+        response = make_response(
+            json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print(result)
+    if result['status'] == '200':
+        #response = make_response(json.dumps('Successfully disconnected.'), 200)
+        #response.headers['Content-Type'] = 'application/json'
+        #return response
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        login_session.pop('logged_in', None)
+        flash('You are now logged out.')
+        return redirect('/index')
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 # Index page
 @app.route('/')
 @app.route('/index')
 def homePage():
-    categories = dbsession.query(Category).all()
+    categories = dbsession.query(Category).distinct()
     latestItems = dbsession.query(Item).order_by(Item.id.desc()).limit(10)
+    #latestItems = dbsession.query(User).distinct().limit(10)
     return render_template(
                            "index.html",
                            categories=categories,
-                           latestItems=latestItems
+                           latestItems=latestItems,
+                           login_session=login_session
                            )
 
+# Show items in a category
+@app.route('/catalog/<category_id>')
+def categoryFullInfo(category_id):
+    categories = dbsession.query(Category).distinct()
+    currentCategory = dbsession.query(Category).filter(Category.id == category_id).one()
+    items = dbsession.query(Item).filter(Item.category_id == category_id).distinct()
+    count = items.count()
+    return render_template(
+                           "categoryDetails.html",
+                           categories=categories,
+                           category_name=currentCategory.name,
+                           items=items,
+                           count=count,
+                           login_session=login_session
+                           )
+
+# Delete item in a category
+@app.route('/catalog/<category_id>/<item_id>/delete', methods=['POST', 'GET'])
+def deleteItem(category_id, item_id):
+
+    itemToDelete = dbsession.query(Item).filter(Item.id == item_id).one()
+    
+    if request.method == 'POST':
+        dbsession.delete(itemToDelete)
+        flash('%s Successfully Deleted' % itemToDelete.title)
+        dbsession.commit()
+        return redirect(url_for('categoryFullInfo',
+                                category_id=itemToDelete.category_id,
+                                login_session=login_session
+                                ))
+    else:
+        return render_template(
+                               'deleteItem.html',
+                               item=itemToDelete,
+                               category_id=itemToDelete.category_id,
+                               login_session=login_session
+                               )
+
+# Create a new item
+@app.route('/catalog/item/new/', methods=['GET', 'POST'])
+def newItem():
+    categories = dbsession.query(Category).distinct()
+    if request.method == 'POST':
+        newItem = Item(
+                       user_id=1,
+                       title=request.form['title'],
+                       description=request.form['description'],
+                       category_id=request.form['category']
+                      )
+        dbsession.add(newItem)
+        dbsession.commit()
+        flash('New Menu %s Item Successfully Created' % (newItem.title))
+        return redirect(url_for('homePage'))
+    else:
+        return render_template(
+                               'addItem.html',
+                               categories=categories,
+                               login_session=login_session
+                               )
+
+# Item Edit
+@app.route('/catalog/<category_id>/<item_id>/edit/', methods=['GET', 'POST'])
+def editItem(category_id, item_id):
+    categories = dbsession.query(Category).distinct()
+    currentCategory = dbsession.query(Category).filter(Category.id == category_id).one()
+    currentItem = dbsession.query(Item).filter(Item.id == item_id).one()
+    if request.method == 'POST':
+        if request.form['title']:
+            currentItem.name = request.form['title']
+        if request.form['description']:
+            currentItem.description = request.form['description']
+        if request.form['category']:
+            currentItem.category_id = request.form['category']
+        dbsession.add(currentItem)
+        dbsession.commit()
+        flash('Item Successfully Edited')
+        return redirect(url_for('homePage'))
+    else:
+        return render_template(
+                               'editItem.html',
+                               categories=categories,
+                               currentCategory=currentCategory,
+                               item=currentItem,
+                               login_session=login_session
+                               )
+
+# Item Description
+@app.route('/description/<category_id>/<item_id>')
+def itemDescription(category_id, item_id):
+    categories = dbsession.query(Category).distinct()
+    item = dbsession.query(Item).filter(Item.id == item_id).one()
+    currentCategory = dbsession.query(Category).filter(Category.id == item.category_id).one()
+    return render_template(
+                           "itemDescription.html",
+                           categories=categories,
+                           category_name=currentCategory.name,
+                           item=item,
+                           login_session=login_session
+                           )
+
+# JSON APIs to view Restaurant Information
+@app.route('/catalog/JSON')
+def catalogJSON():
+    categories = dbsession.query(Category).all()
+    return jsonify(categories=[r.serialize for r in categories])
+
+						   
 if __name__ == '__main__':
     app.debug = True
     app.secret_key = 'super_secret_key'
